9623af7f19bcc645282757da4c7cbeb82de9bbba
Anh Tran <anh.tran@swhwc.com>
2018-11-20 19:09:54 +0700

    tmp

--- STM32/Source/va-x/asp/bootloader/bootloader.c
+++ STM32/Source/va-x/asp/bootloader/bootloader.c
@@ -1,6 +1,7 @@
 #include "bootloader.h"
 #include "kernel_impl.h"
 #include "drviflx.h"
+#include "drvqflx.h"
 
 #if 1
 #define DBGLOG0(msg)                    syslog(LOG_NOTICE, "[BOOTLOADER]" msg)
@@ -20,10 +21,23 @@
 
 #define MAX_BUFFER_SIZE 4096
 
+typedef struct {
+    uint32_t    leb_state;  // 論理イレースブロック状態
+} LEB_HEADER_T;
+
+// データブロックヘッダ
+typedef struct {
+    uint32_t    data_id;    // データID
+    size_t      data_len;   // データ長
+    uint32_t    db_state;   // データブロック状態
+} DB_HEADER_T;
+
 __attribute__ ((section(".ramfunc"))) void boot_load(void)
 {
     //DEBUG
     target_initialize();
+
+    drvqflx_initialize_peripherals();
 
     DBGLOG0("boot_load() starts.");
 
@@ -35,12 +49,26 @@ __attribute__ ((section(".ramfunc"))) void boot_load(void)
     int i;
 
     // TODO Initialize external flash driver
+    drvqflx_initialize();
 
     // TODO Change flash into memory mapped mode
 
     // TODO Get is_new_firmware flag and program size
-    is_new_firmware = true;
+    is_new_firmware = false;
     program_size = 1024;
+    // CODING
+//    intptr_t leb_addr = EFLASH_PROGRAM_AREA_ADDR;
+//    intptr_t db_addr = 0;
+//    DB_HEADER_T* db_header = 0x90000000 | EFLASH_PROGRAM_AREA_ADDR | 0x04;
+//    db_addr = leb_addr + sizeof(LEB_HEADER_T);
+//    DBGLOG3("QFlash Header %d %d %d", db_header->data_id, db_header->data_len, db_header->db_state);
+
+    intptr_t db_addr = EFLASH_PROGRAM_AREA_ADDR;
+    DB_HEADER_T db_header = {0};
+    drvqflx_read(&db_header, db_addr, sizeof(DB_HEADER_T));
+
+    DBGLOG3("QFlash Header %d %d %d", db_header.data_id, db_header.data_len, db_header.db_state);
+
 
     if (is_new_firmware) {
         DBGLOG1("New firmware found (%d)", program_size);
@@ -89,5 +117,5 @@ __attribute__ ((section(".ramfunc"))) void boot_load(void)
         // TODO Clear is_new_firmware flag here (or we could disable it when program started)
     }
 
-    sta_ker();
+    //sta_ker();
 }
